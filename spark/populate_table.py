f4f004731de30460564b6fadb3054f659e6c40a4
Andrey Bozhko <andybozhko@gmail.com>
2018-06-14 14:14:05 -0500

    fix tabs

--- spark/populate_table.py
+++ spark/populate_table.py
@@ -16,10 +16,10 @@ def enforce_schema(msg):
     res["passengers"] = int(fields[7])
     res["time_slot"] = determine_time_slot(fields[5])
     res["block_id"], res["sub_block_id"] = determine_block_ids(lon, lat)
-    
-    
-    if res["block_id"] < 0 or res["time_slot"] < 0:	
-	return
+
+
+    if res["block_id"] < 0 or res["time_slot"] < 0:
+        return
 
     #res["unique_id"] = str(uuid4())
     return res
@@ -45,12 +45,12 @@ if __name__ == '__main__':
 
 
     data = sc.textFile("s3a://{}/{}/*.csv".format(bucketname, foldername))
-    
+
     (data.map(lambda x: enforce_schema(x))
          .filter(lambda x: x is not None)
-	 .map(lambda x: ((x["block_id"], x["time_slot"], x["sub_block_id"]), x["passengers"]))
-	 .reduceByKey(lambda x,y : x+y)
-	 .map(lambda x: ((x[0][0], x[0][1]), [x[0][2], x[1]]))
-	 .groupByKey()
-	 .map(lambda x: {"block_id": x[0][0], "time_slot": x[0][1], "subblock_psgcnt": sorted(x[1], key=lambda z: -z[1])[:10]})
+         .map(lambda x: ((x["block_id"], x["time_slot"], x["sub_block_id"]), x["passengers"]))
+         .reduceByKey(lambda x,y : x+y)
+         .map(lambda x: ((x[0][0], x[0][1]), [x[0][2], x[1]]))
+         .groupByKey()
+         .map(lambda x: {"block_id": x[0][0], "time_slot": x[0][1], "subblock_psgcnt": sorted(x[1], key=lambda z: -z[1])[:10]})
          .saveToCassandra(keyspace, table))

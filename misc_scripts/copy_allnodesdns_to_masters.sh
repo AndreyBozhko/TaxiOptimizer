4931d7456dccd422188bafdc54b1bbcb6c964250
Andrey Bozhko <andybozhko@gmail.com>
2018-06-22 10:32:09 -0500

    add script to export all nodes' addresses to every master's environment

---
+++ misc_scripts/copy_allnodesdns_to_masters.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+output=""
+dest=/home/ubuntu/.profile
+
+clusters=kafka-cluster,spark-batch-cluster,spark-stream-cluster
+
+
+clusters=`echo $clusters | sed s/","/" "/g`
+
+
+for cluster in $clusters ; do
+	
+	i=0
+	CLUSTER=`echo $cluster | tr '[:lower:]' '[:upper:]' | sed s/"-"/"_"/g`
+	for dns in `peg describe $cluster | grep ec2 | sed s/".*DNS: "//g` ; do
+
+		output="$output\nexport ${CLUSTER}_$i=$dns"
+		i=`expr $i + 1`
+	
+	done
+
+done
+
+
+for cluster in $clusters ; do
+
+	peg sshcmd-node $cluster 1 "echo $'$output' >> $dest"
+
+done

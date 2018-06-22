9e29de084fb975c2fb0494c756043c9717efbb44
Andrey Bozhko <andybozhko@gmail.com>
2018-06-22 10:35:56 -0500

    clean up script

--- misc_scripts/copy_idrsapub_to_workers.sh
+++ misc_scripts/copy_idrsapub_to_workers.sh
@@ -2,18 +2,25 @@
 
 line=`cat ~/.ssh/id_rsa.pub`
 
-for cluster in kafka-cluster spark-batch-cluster spark-stream-cluster ; do
+clusters=kafka-cluster,spark-batch-cluster,spark-stream-cluster
 
-  i=0
 
-  for dns in `peg fetch $cluster | grep ec2 | sed s/".*DNS: "/"ubuntu@"/g` ; do
+clusters=`echo $clusters | sed s/","/" "/g`
 
-    if [ `expr $i` -gt 0 ] ; then
-      ssh -i ~/.ssh/*.pem $dns "echo $line >> /home/ubuntu/.ssh/authorized_keys"
-    fi
+for cluster in $clusters ; do
 
-    i=`expr $i + 1`
+	i=0
 
-  done
+	for dns in `peg fetch $cluster | grep ec2 | sed s/".*DNS: "/"ubuntu@"/g` ; do
+
+		if [ `expr $i` -gt 0 ] ; then
+
+			ssh -i ~/.ssh/*.pem $dns "echo $line >> /home/ubuntu/.ssh/authorized_keys"
+
+		fi
+
+		i=`expr $i + 1`
+
+	done
 
 done

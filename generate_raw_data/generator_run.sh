b2fadfe3c230912d91dca85c409ccc3f41008267
Andrey Bozhko <andybozhko@gmail.com>
2018-06-22 13:01:53 -0500

    change to new node's addresses naming convention

--- generate_raw_data/generator_run.sh
+++ generate_raw_data/generator_run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-EC2_MASTER_DNS=`grep ec2 $SPARK_HOME/conf/spark-env.sh | sed s/".*DNS.."//g | sed s/".$"//g`
-
 BUCKET=andrey-bozhko-bucket-insight
 FOLDER=nyc_taxi_raw_data/yellow
 
 for year in 2009 2010 2011 2012 2013 2014 2015 ; do
   for month in 01 02 03 04 05 06 07 08 09 10 11 12 ; do
 
-    spark-submit --master spark://$EC2_MASTER_DNS:7077 \
+    spark-submit --master spark://$SPARK_BATCH_CLUSTER_0:7077 \
                  --executor-memory 4G \
                  generate.py \
                  $year $month $BUCKET $FOLDER
